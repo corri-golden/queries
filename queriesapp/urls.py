07dc55d177532f274fd93562615e1012a4926146
Corri Golden <corri.golden@gmail.com>
2020-03-15 16:44:51 -0500

    update

--- queriesapp/urls.py
+++ queriesapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', query_list, name='home'),
     path('queries/', query_list, name='queries'),
     path('queries/form', queries_form, name='queries_form'),
+    path('queries/<int:query_id>/', query_details, name='query'),
 ]
--- queriesapp/views/__init__.py
+++ queriesapp/views/__init__.py
@@ -1,2 +1,3 @@
 from .queries.list import query_list
 from .queries.form import queries_form
+from .queries.details import query_details
