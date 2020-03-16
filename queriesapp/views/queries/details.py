6568805020f14655def8b843ffc02fac1e814a6e
Corri Golden <corri.golden@gmail.com>
2020-03-16 18:13:19 -0500

    finished delete

--- queriesapp/views/queries/details.py
+++ queriesapp/views/queries/details.py
@@ -14,3 +14,18 @@ def query_details(request, query_id):
         query = get_query(query_id)
         template_name = 'queries/detail.html'
         return render(request, template_name, {'query': query})
+
+    elif request.method == 'POST':
+        form_data = request.POST
+    
+    if (
+        "actual_method" in form_data
+        and form_data["actual_method"] == "DELETE"
+        ):
+
+        query = Query.objects.get(pk=query_id)
+        query.delete()
+
+        return redirect(reverse('queriesapp:queries'))
+
+    
