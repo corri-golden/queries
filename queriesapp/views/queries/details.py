83045b2b34a7fa84c6dc4c129655c709f2569807
Corri Golden <corri.golden@gmail.com>
2020-03-19 16:49:48 -0500

    need to get the value for drop down and edit is complete

--- queriesapp/views/queries/details.py
+++ queriesapp/views/queries/details.py
@@ -1,12 +1,17 @@
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from queriesapp.models import Query
+from queriesapp.models import Query, Agent
 from ..connection import Connection
+
 
 def get_query(query_id):
 
     return Query.objects.get(pk=query_id)
+
+def get_agent(agent_id):
+
+    return Agent.objects.get(pk=agent_id)
 
 # @login_required dd
 def query_details(request, query_id):
@@ -17,6 +22,33 @@ def query_details(request, query_id):
 
     elif request.method == 'POST':
         form_data = request.POST
+
+        # Check if this POST is for editing a book
+    if (
+            "actual_method" in form_data
+            and form_data["actual_method"] == "PUT"
+        ):
+        
+        # # retrieve it first:
+        query_to_update = Query.objects.get(pk=query_id)
+        
+
+        # # Reassign a property's value
+        query_to_update.sent = form_data['sent']
+        query_to_update.expiration = form_data['expiration']
+        query_to_update.notes = form_data['notes']
+        query_to_update.agent_id = form_data['agent_id']
+         # # Save the change to the db
+        query_to_update.save()
+
+        # # retrieve it first:
+        agent_to_update = Agent.objects.get(pk=query_to_update.agent_id)
+        agent_to_update.email = form_data['email']
+        agent_to_update.company = form_data['company']
+        
+        agent_to_update.save()
+
+        return redirect(reverse('queriesapp:queries'))
     
     if (
         "actual_method" in form_data
