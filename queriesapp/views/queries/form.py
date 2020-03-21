1284b975da781973219167c22631581a4bbe2372
Corri Golden <corri.golden@gmail.com>
2020-03-21 08:44:34 -0500

    finished queries dropdown

--- queriesapp/views/queries/form.py
+++ queriesapp/views/queries/form.py
@@ -1,10 +1,11 @@
 import sqlite3
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from queriesapp.models import Query
+from queriesapp.models import Query, Book
 from ..connection import Connection
 from queriesapp.models import Agent
 from .details import get_query
+
 
 def get_query(query_id):
 
@@ -15,15 +16,21 @@ def get_agents():
     all_agents = Agent.objects.all()
     return all_agents
 
+def get_books():
+    all_books = Book.objects.all()
+    return all_books
+
 
 
 # @login_required
 def queries_form(request):
     if request.method == 'GET':
         agents = get_agents()
+        books = get_books()
         template = 'queries/form.html'
         context = {
-            'all_agents': agents
+            'all_agents': agents,
+            'all_books': books
         }
 
     return render(request, template, context)
